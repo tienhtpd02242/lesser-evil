96ebed6ff7e63e27113606b492a1e5d280b1c514
tienhtpd02242 <tienhtpd02242@gmail.com>
2022-07-23 09:07:39 +0700

    update theme

---
+++ package-main/functions.php
@@ -0,0 +1,35 @@
+<?php
+/*
+* Function
+*/
+use ScssPhp\ScssPhp\Compiler;
+
+if( ! function_exists( 'pj_scss_compiler' ) ) {
+    /**
+     * Scss Compiler
+     *
+     * @param string $in
+     * @param string $out
+     * @param string $import_path
+     * @param string $formatter (default: ScssPhp\ScssPhp\Formatter\Compressed)
+     * @param boolean $source_map (SOURCE_MAP_INLINE)
+     *
+     * @return void
+     */
+    function pj_scss_compiler( $scss_string, $out, $import_path = '', $formatter = 'ScssPhp\ScssPhp\Formatter\Compressed', $source_map = false ) {
+        $scss = new Compiler();
+        if( ! empty( $import_path ) ) $scss->setImportPaths( $import_path );
+        if( ! empty( $formatter ) ) $scss->setFormatter( $formatter );
+        if( true == $source_map ) $scss->setSourceMap( Compiler::SOURCE_MAP_INLINE );
+        file_put_contents( $out, $scss->compile( $scss_string ) );
+    }
+}
+
+function package_main_types_to_uploads($file_types){
+  $new_filetypes = array();
+  $new_filetypes['svg'] = 'image/svg+xml';
+  $file_types = array_merge($file_types, $new_filetypes );
+
+  return $file_types;
+}
+add_filter('upload_mimes', 'package_main_types_to_uploads');
---
+++ page-templates/homepage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Template Name: Home Page
+*
+* @package WordPress
+*
+*/
+get_header();
+
+?>
+<h1>This is Homepage</h1>
+<?php
+
+get_footer();
